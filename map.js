c5469e3be78f61b9ed6078a87f0dec6523be5d8c
RAS705 <95494614+RAS705@users.noreply.github.com>
2022-03-18 13:35:20 -0500

    initial commit to branch

---
+++ map.js
@@ -0,0 +1,23 @@
+
+//var map = L.map('map').setView([51.505, -0.09], 13);
+let map = L.map("map", {
+    center: [
+      40.7, -94.5
+    ],
+    zoom: 4
+  });
+
+console.log('Key: ', API_KEY);
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+//}).addTo(map);
+
+// Then we add our 'graymap' tile layer to the map.
+streets.addTo(map);
