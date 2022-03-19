fd48665ca75bffa7ebd903b14b767fb916ea98cb
RAS705 <95494614+RAS705@users.noreply.github.com>
2022-03-19 10:46:17 -0500

    Update map.js

--- static/js/map.js
+++ static/js/map.js
@@ -4,15 +4,31 @@ let map = L.map("map", {
     center: [
       30.3, -87.7
     ],
-    zoom: 11
+    zoom: 14
   });
 
 console.log('Key: ', API_KEY);
+
+//Add a marker for my house
+//var marker = L.marker([30.31289, -87.65957]).addTo(map);
+
+L.circle([30.31289, -87.65957], {
+  radius: 300,
+  color:"black",
+  fillColor: "yellow"
+}).addTo(map);
+
+//L.circleMarker([30.31289, -87.65957], {
+//  radius: 300,
+//  color:"black",
+//  fillColor: "yellow"
+//}).addTo(map);
 
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox/streets-v11',
+    //id: 'mapbox/dark-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
