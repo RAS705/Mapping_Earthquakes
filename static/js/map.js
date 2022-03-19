22d2ba84a8cec7d94513428dc7c38c23050eb86f
RAS705 <95494614+RAS705@users.noreply.github.com>
2022-03-19 11:31:29 -0500

    Update map.js to plot multiple cities

--- static/js/map.js
+++ static/js/map.js
@@ -4,13 +4,15 @@ let map = L.map("map", {
     center: [
       30.3, -87.7
     ],
-    zoom: 14
+    zoom: 10
   });
 
 console.log('Key: ', API_KEY);
 
 //Add a marker for my house
-//var marker = L.marker([30.31289, -87.65957]).addTo(map);
+var marker = L.marker([30.31289, -87.65957])
+.bindPopup("<h2>Gulf Shores, AL</h2> <hr> <h3>My House</h3>")
+.addTo(map);
 
 L.circle([30.31289, -87.65957], {
   radius: 300,
@@ -37,3 +39,17 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+//Load city data from the Cities.js file
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city.
+cityData.forEach(function(city) {
+  console.log(city)
+  //L.marker(city.location)
+  L.circleMarker(city.location, {
+    radius: city.population/100000
+  })
+  .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
+  .addTo(map);
+ });
