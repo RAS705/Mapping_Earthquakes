66b4ff55d70aa7861f99428ecbfadf486877cee2
RAS705 <95494614+RAS705@users.noreply.github.com>
2022-03-19 12:12:21 -0500

    Update map.js

--- static/js/map.js
+++ static/js/map.js
@@ -29,7 +29,8 @@ L.circle([30.31289, -87.65957], {
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    //id: 'mapbox/streets-v11',
+    id: 'mapbox/satellite-streets-v11',
     //id: 'mapbox/dark-v10',
     tileSize: 512,
     zoomOffset: -1,
@@ -39,6 +40,18 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+// Coordinates for each point to be used in the polyline.
+var line = [
+  [[37.6213, -122.3790], [30.1944, -97.67], [43.6835, -79.6149], [40.6372, -73.7788]],
+  [[33.9416, -118.4085], [37.6213, -122.3790], [40.7899, -111.9791], [47.4502, -122.3088]
+  ]
+];
+
+// Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+  color: "blue"
+}).addTo(map);
 
 //Load city data from the Cities.js file
 let cityData = cities;
