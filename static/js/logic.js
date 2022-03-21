ae86d8b5d5c23887eea7c43f73cc70dfbbb99451
RAS705 <95494614+RAS705@users.noreply.github.com>
2022-03-21 10:05:17 -0500

    Update logic.js to read Toronto routes data

--- static/js/logic.js
+++ static/js/logic.js
@@ -13,8 +13,8 @@ let dark = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
-  id: 'mapbox/streets-v11',
-  //id: 'mapbox/light-v10',
+  //id: 'mapbox/streets-v11',
+  id: 'mapbox/light-v10',
   //id: 'mapbox/satellite-streets-v11',
   //id: 'mapbox/dark-v10',
   tileSize: 512,
@@ -36,7 +36,7 @@ let baseMaps = {
 // Create the map object with center and zoom level.
 let map = L.map('map',{
   center: [
-    30, 30
+    44.0, -80.0
   ],
   zoom: 2,
   layers: [streets]
@@ -53,6 +53,17 @@ let map = L.map('map',{
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
+
+// Accessing the Toronto airline routes GeoJSON URL.
+let torontoData = "https://raw.githubusercontent.com/RAS705/Mapping_Earthquakes/main/static/js/torontoRoutes.json";
+
+// Grabbing our GeoJSON data.
+d3.json(torontoData).then(function(data) {
+  console.log(data);
+// Creating a GeoJSON layer with the retrieved data.
+L.geoJSON(data).addTo(map);
+});
+
 
 // Accessing the airport GeoJSON URL
 let airportData = "https://raw.githubusercontent.com/RAS705/Mapping_Earthquakes/main/static/js/majorAirports.json";
